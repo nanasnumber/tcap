dd256b76bcad7f11438e718cb03611b2330e304f
enlezLab <enlezlab@gmail.com>
2023-02-17 11:15:44 -0500

    Working prototype

--- main.go
+++ main.go
@@ -6,8 +6,20 @@ import (
 	"strings"
 )
 
+func wordList() []string {
+	r := []string{
+		"a",
+		"an",
+		"is",
+	}
+	return r
+}
+
 func input() string {
+	// get input from terminal
 	r := os.Args[1]
+	// remove redundant spaces
+	r = strings.Join(strings.Fields(r), " ")
 	return r
 }
 
@@ -26,14 +38,35 @@ func capital(word string) string {
 	return r
 }
 
+func lower(word string) string {
+	var arr []string
+	for _, w := range word {
+		arr = append(arr, string(w))
+	}
+	arr[0] = strings.ToLower(arr[0])
+	r := strings.Join(arr, "")
+	return r
+}
+
 func ignore(s string) bool {
 	// check if word is within ignore list for title case
+	list := wordList()
+	for _, wl := range list {
+		if s == wl {
+			return true
+		}
+	}
 	return false
 }
 
 func titleCase(word []string) string {
 	for idx, w := range word {
-		word[idx] = capital(w)
+		if idx == 0 {
+			word[idx] = capital(w)
+		}
+		if ignore(w) != true && idx != 0 {
+			word[idx] = capital(w)
+		}
 	}
 	r := strings.Join(word, " ")
 	return r
@@ -44,4 +77,5 @@ func main() {
 	words := words(input)
 
 	fmt.Println(titleCase(words))
+	fmt.Println(wordList())
 }
